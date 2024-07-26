322ee9d44d67c7f7ebc1636e7b198d56b10fc719
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2024-07-25 23:47:43 -0600

    first commit

---
+++ src/NavBar.tsx
@@ -0,0 +1,20 @@
+import "./NavBar.css";
+function NavBar() {
+  return (
+    <div className="nav-bar">
+      <div>
+        <h1>Alysa Punjwani</h1>
+      </div>
+      <div className="flex">
+        <h4>EXPERIENCE</h4>
+        <h4>EDUCATION</h4>
+        <h4>CERTIFICATIONS</h4>
+        <h4>SOFTWARE</h4>
+        <h4>CONTACT</h4>
+        <h4>PORTFOLIO</h4>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
